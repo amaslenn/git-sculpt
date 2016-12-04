e939accd456876bd954bbafdaf75a5c74664ccce
Maslennikov, Andrey <andrew.maslennikov@gmail.com>
2016-12-04 20:12:46 +0300

    Implement getLocalBranches()

--- git-bleach.go
+++ git-bleach.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 import "flag"
+import "os/exec"
+import "bytes"
+import "strings"
+import "os"
 
 var dryRun bool
 var baseCommit string
@@ -12,6 +16,31 @@ func init() {
     flag.StringVar(&baseCommit, "base", "master", "Base branch or commit")
 }
 
+// funtion returns all local branches which do not have upstream
+func getLocalBranches() (branches []string, err error) {
+    // this super easy format was choosen to exclude any unexpected symbols in output
+    cmd := exec.Command("git", "for-each-ref", "--format=%(refname:short)", "refs/heads/")
+    var out bytes.Buffer
+    cmd.Stdout = &out
+
+    err = cmd.Run()
+    if err != nil {
+        return branches, err
+    }
+
+    all_branches := strings.Split(out.String(), "\n")
+
+    for i := 0; i < len(all_branches); i++ {
+        cmd = exec.Command("git", "rev-parse", "--symbolic-full-name", all_branches[i] + "@{u}")
+        loc_err := cmd.Run()
+        if loc_err != nil {     // means there is no upstream branch
+            branches = append(branches, all_branches[i])
+        }
+    }
+
+    return branches, err
+}
+
 func main() {
     flag.Parse()
     var argsTail = flag.Args()
@@ -19,8 +48,14 @@ func main() {
         localBranch = argsTail[0]
     }
 
+    branches, err := getLocalBranches()
+    if err != nil {
+        fmt.Println("ERROR:", err)
+        os.Exit(1)
+    }
+
     fmt.Println("Dry run:", dryRun)
     fmt.Println("Base commit:", baseCommit)
     fmt.Println("Local branch:", localBranch)
+    fmt.Println("Local branches:", branches)
 }
-
