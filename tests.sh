9acaec76924da2481e683d7b05c2643d35f0f710
Maslennikov, Andrey <andrew.maslennikov@gmail.com>
2017-02-09 23:07:00 +0300

    Improve testing for Travis CI

--- tests.sh
+++ tests.sh
@@ -4,6 +4,8 @@ rm -rf .tests
 mkdir .tests
 cd .tests
 git init
+git config user.name "tester"
+git config user.email "tester@test.test"
 touch > foo
 
 # 1 commit
@@ -29,12 +31,14 @@ git commit -am "change foo"
 PATH=$PATH:../build/amd64/linux/
 git checkout master
 
+error=0
 echo "t: empty branch"
 git sculpt -d br-empty
 if [ $? -eq 0 ]; then
     echo PASSED
 else
     echo FAILED
+    error=$((error+1))
 fi
 
 echo "t: merged branch"
@@ -43,13 +47,17 @@ if [ $? -eq 0 ]; then
     echo PASSED
 else
     echo FAILED
+    error=$((error+1))
 fi
 
 echo "t: branch with changes"
 git sculpt -d br-new
 if [ $? -eq 0 ]; then
     echo FAILED
+    error=$((error+1))
 else
     echo PASSED
 fi
+
+exit $error
 
